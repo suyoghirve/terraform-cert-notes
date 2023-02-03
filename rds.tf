b8293e7578a3647e5a72a850c03e2328e1e3450e
suyog.hirve <suyog.hirve@druva.com>
2023-02-03 12:16:27 +0530

    update

--- rds.tf
+++ rds.tf
@@ -5,7 +5,7 @@ resource "aws_db_instance" "default" {
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
   db_name              = "mydb"
-  username             = "foo"
+  username             = "mydatabase"
   password             = "${file("../rds_pass.txt")}"
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot = "true"
