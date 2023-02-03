d4762aac8c80e6053358a9d45c71162c41e1276c
suyog.hirve <suyog.hirve@druva.com>
2023-02-03 13:23:22 +0530

    update

--- Terraform/RDS/providers.tf
+++ Terraform/RDS/providers.tf
@@ -5,6 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket        = "bysystem-tm"
+    key  = "demo.tfstate"
     dynamodb_table = "terraform_statelocking"
     region        = "ap-south-1"
   }
