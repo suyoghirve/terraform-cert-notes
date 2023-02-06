a1892462e060699e35afa5cf04332c16b1dbe6af
suyog.hirve <suyog.hirve@druva.com>
2023-02-06 22:27:55 +0530

    update

--- Terraform/modules/all-modules/ec2.tf
+++ Terraform/modules/all-modules/ec2.tf
@@ -1,7 +1,7 @@
 
 resource "aws_instance" "web2" {
   ami           = "ami-0cca134ec43cf708f"
-  instance_type = "t2.micro"
+  instance_type = "t2.large"
   tags = {
     Name = "terraform-module"
   }     
