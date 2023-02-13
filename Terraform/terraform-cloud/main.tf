2cfd84943238cdac18f134e9176c1401da9d48f0
suyog.hirve <suyog.hirve@druva.com>
2023-02-13 21:11:21 +0530

    update

--- Terraform/terraform-cloud/main.tf
+++ Terraform/terraform-cloud/main.tf
@@ -17,8 +17,8 @@ terraform {
 
 provider "aws" {
   region     = "ap-south-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+ # access_key = var.aws_access_key   Add these Access/ Secret Access Keys in Terraform Cloud Workspace as env variable.
+  #secret_key = var.aws_secret_key
 }
 resource "aws_instance" "web2" {
   ami           = "ami-0cca134ec43cf708f"
--- Terraform/terraform-cloud/variable.tf
+++
@@ -1,8 +0,0 @@
-variable "aws_access_key" {
-  type = string
-  default = ""
-}
-variable "aws_secret_key" {
-  type = string
-  default = ""
-}
