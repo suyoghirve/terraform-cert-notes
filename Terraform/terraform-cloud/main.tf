c0dbdfd60c7f364c3e14a358680a4afa378edd1d
suyog.hirve <suyog.hirve@druva.com>
2023-02-13 20:09:00 +0530

    update

--- Terraform/terraform-cloud/main.tf
+++ Terraform/terraform-cloud/main.tf
@@ -16,6 +16,8 @@ terraform {
 }
 provider "aws" {
   region     = "ap-south-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 resource "aws_instance" "web2" {
   ami           = "ami-0cca134ec43cf708f"
