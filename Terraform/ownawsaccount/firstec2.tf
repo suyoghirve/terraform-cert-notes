df655a9f50ca667086e961c5fa3b1ec326778d9f
suyog.hirve <suyog.hirve@druva.com>
2023-02-16 16:43:57 +0530

    update

--- Terraform/ownawsaccount/firstec2.tf
+++ Terraform/ownawsaccount/firstec2.tf
@@ -36,7 +36,7 @@ resource "aws_s3_bucket_versioning" "bysystem-tm" {
 resource "aws_instance" "web2" {
   ami           = "ami-0dfcb1ef8550277af"
   instance_type = "t2.micro"
-  count         = 2
+  count         = 1
   tags = {
     #Name = "HelloWorld-renew.${count.index}"  # this will create 3 ec2 with same name
     Name = var.ec2-name[count.index]
