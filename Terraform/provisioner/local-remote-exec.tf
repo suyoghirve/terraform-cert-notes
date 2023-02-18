b3793c08a30f469f41b85ff9d20b4d6578b752f3
suyog.hirve <suyog.hirve@druva.com>
2023-02-18 14:13:10 +0530

    update

--- Terraform/provisioner/local-remote-exec.tf
+++ Terraform/provisioner/local-remote-exec.tf
@@ -29,7 +29,7 @@ resource "aws_instance" "web2" {
     connection {
     type     = "ssh"
     user     = "ec2-user"
-    private_key = "file(ownkey.pem)"
+    private_key = "file(./ownkey.pem)"
     host     = self.public_ip
   }
 
