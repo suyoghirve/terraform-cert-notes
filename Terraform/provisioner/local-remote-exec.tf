253f178d0d3ff3ea16c5a3c0af602bd38e562248
suyog.hirve <suyog.hirve@druva.com>
2023-02-18 14:20:49 +0530

    update

--- Terraform/provisioner/local-remote-exec.tf
+++ Terraform/provisioner/local-remote-exec.tf
@@ -25,7 +25,30 @@ resource "aws_instance" "web2" {
   tags = {
     Name = "terraform-provisioner"
   }
-    
+  resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-05c9a0905597d37d9"
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
     connection {
     type     = "ssh"
     user     = "ec2-user"
