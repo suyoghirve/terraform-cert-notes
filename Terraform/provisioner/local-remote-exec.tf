673c7ab1998ca00a4d3d6090c641cd7b1d19572c
suyog.hirve <suyog.hirve@druva.com>
2023-02-22 17:28:14 +0530

    update

--- Terraform/provisioner/local-remote-exec.tf
+++ Terraform/provisioner/local-remote-exec.tf
@@ -34,7 +34,7 @@ resource "aws_security_group" "allow_ssh" {
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
   }
-
+}
 resource "aws_instance" "web2" {
   ami           = "ami-0dfcb1ef8550277af"
   instance_type = "t2.micro"
