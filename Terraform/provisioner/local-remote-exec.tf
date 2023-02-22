b5b360795ad51da5aafe838ec563c87b0c661ce7
suyog.hirve <suyog.hirve@druva.com>
2023-02-22 17:31:01 +0530

    update

--- Terraform/provisioner/local-remote-exec.tf
+++ Terraform/provisioner/local-remote-exec.tf
@@ -61,7 +61,8 @@ resource "aws_instance" "web2" {
     inline = [
       "sudo yum update -y",
       "sudo amazon-linux-extras install nginx1 -y",
-      "sudo systemctl start nginx"
+      "sudo systemctl start nginx",
+      "sudo systemctl enable nginx"
     ]
   }
 }
