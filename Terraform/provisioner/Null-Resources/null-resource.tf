6ff3ef5b85e3ec1867d91cfccfcd6ea907cadbe6
suyog.hirve <suyog.hirve@druva.com>
2023-02-27 18:43:13 +0530

    update

--- Terraform/provisioner/Null-Resources/null-resource.tf
+++ Terraform/provisioner/Null-Resources/null-resource.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-     #source  = "hashicorp/aws"
-      #version = "4.51.0"
+      source  = "hashicorp/aws"
+      version = "4.51.0"
       source = "hashicorp/null"
       version = "3.2.1"
     }
