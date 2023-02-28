710cffd4da5bd5330267a6a7dd9ddead14f7ea58
suyog.hirve <suyog.hirve@druva.com>
2023-02-28 18:29:05 +0530

    update

---
+++ Terraform/ALB/vpc-subnet.tf
@@ -0,0 +1,44 @@
+resource "aws_vpc" "custom_vpc" {
+  cidr_block           = var.custom_vpc
+  instance_tenancy     = var.instance_tenancy
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+resource "aws_subnet" "public_subnet" {
+  count             = var.custom_vpc == "10.0.0.0/16" ? 3 : 0
+  vpc_id            = aws_vpc.custom_vpc.id
+  availability_zone = data.aws_availability_zones.azs.names[count.index]
+  cidr_block        = element(cidrsubnets(var.custom_vpc, 8, 4, 4), count.index)
+
+  tags = {
+    "Name" = "Public-Subnet-${count.index}"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  tags = {
+    "Name" = "Internet-Gateway"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  tags = {
+    "Name" = "Public-RouteTable"
+  }
+}
+
+resource "aws_route" "public_route" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public_rt_association" {
+  count          = length(aws_subnet.public_subnet) == 3 ? 3 : 0
+  route_table_id = aws_route_table.public_rt.id
+  subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
+}
