8c30221c13e9ed173b0bec95c905ec014a844967
suyog.hirve <suyog.hirve@druva.com>
2023-02-28 18:31:44 +0530

    update

---
+++ Terraform/ALB/ec2.tf
@@ -0,0 +1,46 @@
+resource "aws_instance" "instance" {
+  count                = length(aws_subnet.public_subnet.*.id)
+  ami                  = var.ami_id
+  instance_type        = var.instance_type
+  subnet_id            = element(aws_subnet.public_subnet.*.id, count.index)
+  security_groups      = [aws_security_group.sg.id, ]
+  key_name             = "Keypair-01"
+  iam_instance_profile = data.aws_iam_role.iam_role.name
+
+  tags = {
+    "Name"        = "Instance-${count.index}"
+    "Environment" = "Test"
+    "CreatedBy"   = "Terraform"
+  }
+
+  timeouts {
+    create = "10m"
+  }
+
+}
+
+resource "null_resource" "null" {
+  count = length(aws_subnet.public_subnet.*.id)
+
+  provisioner "file" {
+    source      = "./userdata.sh"
+    destination = "/home/ec2-user/userdata.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /home/ec2-user/userdata.sh",
+      "sh /home/ec2-user/userdata.sh",
+    ]
+    on_failure = continue
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    port        = "22"
+    host        = element(aws_eip.eip.*.public_ip, count.index)
+    private_key = file(var.ssh_private_key)
+  }
+
+}
