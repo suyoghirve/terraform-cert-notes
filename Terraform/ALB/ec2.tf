1a76a893165a6bee6f16bbe77cd1b78c8a19ce46
suyog.hirve <suyog.hirve@druva.com>
2023-02-28 19:12:38 +0530

    update

--- Terraform/ALB/ec2.tf
+++ Terraform/ALB/ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "instance" {
   instance_type        = var.instance_type
   subnet_id            = element(aws_subnet.public_subnet.*.id, count.index)
   security_groups      = [aws_security_group.sg.id, ]
-  key_name             = "Keypair-01"
+  key_name             = "ownkey"
   #iam_instance_profile = data.aws_iam_role.iam_role.name
 
   tags = {
