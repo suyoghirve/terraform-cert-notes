46bdb29a2df4d4be6552f3fbecb07d1552b7a051
suyog.hirve <suyog.hirve@druva.com>
2023-03-06 14:15:23 +0530

    update

---
+++ Terraform/ALB/data.tf
@@ -0,0 +1,27 @@
+data "aws_elb_service_account" "main" {}
+
+resource "aws_s3_bucket" "elb_logs" {
+  bucket = "my-elb-tf-test-bucket"
+  acl    = "private"
+
+  policy = <<POLICY
+{
+  "Id": "Policy",
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:PutObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::my-elb-tf-test-bucket/AWSLogs/*",
+      "Principal": {
+        "AWS": [
+          "${data.aws_elb_service_account.main.arn}"
+        ]
+      }
+    }
+  ]
+}
+POLICY
+}
