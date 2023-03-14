702a5d2a976fefc133b2da560359145ffee9471f
suyog.hirve <suyog.hirve@druva.com>
2023-03-14 12:56:04 +0530

    update

--- Terraform/RDS/rds.tf
+++ Terraform/RDS/rds.tf
@@ -14,7 +14,7 @@ resource "aws_db_instance" "default" {
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
   username             = "foo"
-  password             = "${file("../rds_pass.txt")}"
+  password             =file("../rds_pass.txt")  #"${file("../rds_pass.txt")}"
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
 }
